ffdf15e37c7e8fb661c80f6a69a3dd1ecf8df139
Ubuntu <ubuntu@ip-172-31-16-102.us-west-2.compute.internal>
2018-01-05 23:32:12 +0000

    fixed typo

--- node/server.js
+++ node/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send('Hi, World!\n);
+	res.send('Hi, World!\n');
 	console.log('Hi, Console!\n');
 });
 
