403ce7e927ac5522e57eaddca0b550061a813dc3
alexbuechele <alexbuechele@gmail.com>
2018-02-22 16:22:37 -0800

    mvc stuff

--- node/db/index.js
+++ node/db/index.js
@@ -2,7 +2,11 @@
 
 const { Pool } = require('pg');
 
+const pool = new Pool();
+
 module.exports = {
-  query: (text, params) => pool.query(text, params)
+	query: (text, params, callback) => {
+		return pool.query(text, params, callback);
+	}
 };
 
