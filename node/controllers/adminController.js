3859e6044d5a86dcfd4bc457b8c6013eda1580aa
Alex <alexbuechele@gmail.com>
2019-04-12 22:54:15 -0700

    hello old friend

--- node/controllers/adminController.js
+++ node/controllers/adminController.js
@@ -1,25 +1,31 @@
-exports.admin = function (req, res, next) {
-    
-    // TODO check for login, render login page or admin panel
-    if (1 == 2){
-        res.render('adminlogin');
-    }
-    else if (1 == 1){
-        // let's bring up the last 10 visitors
-        recentMessages= db.query("SELECT * FROM message_logs;", 
+message_logs_query = (cb) => {
+    db.query("SELECT * FROM message_logs;", 
             (err, result) => {
                 if (err){
                     console.log(err);
                 }
                 else {
-                    for (x in result.rows){
-                        console.log(result.rows[x].name);
-                    }
+                    cb(result.rows);
                 }
         });
-        console.log(recentMessages);
-        res.render('admin');
+};
+
+exports.admin = function (req, res, next) {
+    
+    function format_logs(message_rows) {
+        res.send(message_rows);
+        //res.render('admin');
         //res.render('admin', {recentmessages: recentMessages});
+    }
+
+
+    // TODO check for login, render login page or admin panel
+    if (1 == 2){
+        res.render('adminlogin');
+    }
+    else if (1 == 1){
+        message_logs_query(format_logs);
+
     }
     next();
 }
